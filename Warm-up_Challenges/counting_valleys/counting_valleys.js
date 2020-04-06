8f2e7c4936745f30c7d8570475c062c65d5f5673
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-04-06 23:40:44 +0300

    counting valleys

---
+++ Warm-up_Challenges/counting_valleys/counting_valleys.js
@@ -0,0 +1,18 @@
+function countingValleys(n, s) {
+  let countValley = 0;
+  let seaLevel = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    if (seaLevel == 0 && s[i] == "D") {
+      countValley++;
+    }
+    if (s[i] == "D") {
+      seaLevel--;
+    }
+    if (s[i] == "U") {
+      seaLevel++;
+    }
+  }
+  return countValley;
+}
+cconsole.log(countingValleys(8, "UDDDUDUU"));
+console.log(countingValleys(12, "DDUUDDUDUUUD"));
