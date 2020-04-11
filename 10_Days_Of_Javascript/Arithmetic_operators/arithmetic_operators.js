e3120ca74d4f718b839299b590533610df826bf5
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-04-11 23:41:10 +0300

    arithmetic_operators

---
+++ 10_Days_Of_Javascript/Arithmetic_operators/arithmetic_operators.js
@@ -0,0 +1,11 @@
+function getArea(length, width) {
+    let area =  length * width
+    return area;
+}
+getArea(3, 4.5);
+
+function getPerimeter(length, width) {
+    let perimeter = 2 *(length+width)
+    return perimeter;
+}
+getPerimeter(3, 4.5);
