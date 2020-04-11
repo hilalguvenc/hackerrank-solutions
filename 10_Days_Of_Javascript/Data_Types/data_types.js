62a7522425fca1a5286bd3a42824589fc5b9925b
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-04-11 23:29:04 +0300

    10 Days of Javascript

---
+++ 10_Days_Of_Javascript/Data_Types/data_types.js
@@ -0,0 +1,11 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+  const firstInteger = 4;
+  console.log(firstInteger + parseInt(secondInteger));
+
+  const firstDecimal = 4.0;
+  console.log(firstDecimal + parseFloat(secondDecimal));
+
+  const firstString = "HackerRank ";
+  console.log(firstString + secondString);
+}
+//secondInteger, secondDecimal, secondString = 12,4.32,is the best place to learn and practice coding!
